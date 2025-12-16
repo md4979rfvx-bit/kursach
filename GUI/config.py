214c11037e0b6bd05d3604ac800c11edd64845d9
Алёна <md4979rfvx@privaterelay.appleid.com>
2025-12-16 18:33:24 +0300

    Create config.py

---
+++ GUI/config.py
@@ -0,0 +1,7 @@
+DB_CONFIG = {
+    'host': 'localhost',
+    'database': 'audiotech_db',  # название базы данных
+    'user': 'postgres',
+    'password': '',  # ваш рабочий пароль
+    'port': ''
+}
